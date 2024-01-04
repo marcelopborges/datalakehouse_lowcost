83d171c0c9aa118ccafed80f7ad395d5b8fdb8c1
marcelopborges <54992376+marcelopborges@users.noreply.github.com>
2024-01-04 13:01:47 -0300

    refactor(class): correcao dos erros nos testes do transforms

---
+++ tests/test_transform.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+import pytest
+from elt.transformer import Transformer
+import pandas as pd
+from unittest.mock import patch, mock_open
+import os
+
+
+@pytest.fixture
+def example_data():
+    return {
+        "dados": [
+            {"coluna1": 1, "coluna2": 2},
+            {"coluna1": 3, "coluna2": 4}
+        ]
+    }
+
+def test_convert_data_with_valid_input(example_data):
+    transformer = Transformer("test_dna", example_data)
+    result = transformer.convert_data()
+    today = datetime.today().strftime('%Y%m%d')
+    expected_file_path = os.path.join('tmp', f'test_dna_{today}.parquet')
+    assert result == expected_file_path
+
+
+def test_convert_data_with_valid_input(example_data):
+    transformer = Transformer("test_dna", example_data)
+    result = transformer.convert_data()
+    today = datetime.today().strftime('%Y%m%d')
+    expected_file_path = os.path.join('tmp', f'test_dna_{today}.parquet')
+    assert result == expected_file_path
+
+
+@patch("os.path.exists")
+@patch("os.makedirs")
+@patch("pandas.DataFrame.to_parquet")
+def test_save_as_parquet(mock_to_parquet, mock_makedirs, mock_path_exists, example_data):
+    mock_path_exists.return_value = False
+
+    transformer = Transformer("test_dna", example_data)
+    transformer.convert_data()
+
+    mock_makedirs.assert_called_once_with('tmp')
+    mock_to_parquet.assert_called()
