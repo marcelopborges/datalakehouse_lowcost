088ff04cda022b3d00eb621ac758758200f4953b
marcelopborges <54992376+marcelopborges@users.noreply.github.com>
2024-01-04 11:07:25 -0300

    fix(class): feito diversas correcoes devido a mudanca no servidor webservices ter alterado a politica de seguranca colocando login e senha

--- elt/transformer.py
+++ elt/transformer.py
@@ -1,9 +1,37 @@
-class Transformer():
-    def __init__(self):
-        return
+import json
+import os
+import pandas as pd
+from datetime import datetime
+import io
 
-    def clean_data(self, data):
-        pass
+class Transformer:
+    def __init__(self, nome_dna, raw_data):
+        self.raw_data = raw_data
+        self.nome_dna = nome_dna
+        self.today = datetime.today().strftime('%Y%m%d')
 
-    def normalize_data(self, data):
-        pass
+    def convert_data(self):
+        # Verifica se os dados estão no formato esperado
+        if not isinstance(self.raw_data, dict) or 'dados' not in self.raw_data:
+            raise ValueError("Formato JSON inválido ou chave 'dados' não encontrada")
+
+        # Converte os dados JSON para DataFrame
+        df = pd.DataFrame(self.raw_data['dados'])
+
+        # Salva o DataFrame como um arquivo Parquet
+        parquet_file_path = self.save_as_parquet(df)
+
+        return parquet_file_path
+
+    def save_as_parquet(self, df):
+        # Verifica se o diretório 'tmp' existe, se não, cria
+        if not os.path.exists('tmp'):
+            os.makedirs('tmp')
+
+        # Monta o caminho do arquivo Parquet
+        file_path = os.path.join('tmp', f'{self.nome_dna}_{self.today}.parquet')
+
+        # Salva o DataFrame como Parquet
+        df.to_parquet(file_path)
+
+        return file_path
