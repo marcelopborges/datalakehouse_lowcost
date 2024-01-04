03af410e1184e1f5119a32e5f9a48895faec6ddb
marcelopborges <54992376+marcelopborges@users.noreply.github.com>
2024-01-04 11:41:48 -0300

    fix(class): correcao dos tests do sianet extractor

--- tests/test_sianet_extractor.py
+++ tests/test_sianet_extractor.py
@@ -14,7 +14,8 @@ def mock_auth_token():
 def test_sianet_extractor_data_dna_km_rodado(mock_get, mock_auth_token):
     test_date = '01/01/2023'
     expected_response_data = {'result': 'success'}
-    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA=dna_fake&data={test_date}'
+    name_dna = 'dna_kmRodado'
+    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA={name_dna}&data={test_date}'
     expected_headers = {'Authorization': 'Bearer fake_token'}
 
     response_mock = MagicMock()
@@ -22,18 +23,20 @@ def test_sianet_extractor_data_dna_km_rodado(mock_get, mock_auth_token):
     response_mock.json.return_value = expected_response_data
     mock_get.return_value = response_mock
 
-    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/WSLogin', nome_dna='dna_fake')
+    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/BI_Extrator', nome_dna=name_dna)
     actual_response = sianet_extractor.extract_km_rodado(test_date)
 
     mock_get.assert_called_once_with(expected_url, headers=expected_headers)
     assert actual_response == expected_response_data
+    assert 'Authorization' in mock_get.call_args.kwargs['headers']
 
 
 @patch("requests.get")
 def test_extract_dna_linha(mock_get, mock_auth_token):
     test_date = '01/01/2023'
     expected_response_data = {'result': 'success'}
-    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA=dna_fake&data={test_date}'
+    name_dna = 'dna_linha'
+    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?data_inicio={test_date}&data_final={test_date}&nomeDNA={name_dna}'
     expected_headers = {'Authorization': 'Bearer fake_token'}
 
     response_mock = MagicMock()
@@ -41,7 +44,7 @@ def test_extract_dna_linha(mock_get, mock_auth_token):
     response_mock.json.return_value = expected_response_data
     mock_get.return_value = response_mock
 
-    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/WSLogin', nome_dna='dna_fake')
+    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/BI_Extrator', nome_dna=name_dna)
     actual_response = sianet_extractor.extract_dna_linha(test_date)
 
     mock_get.assert_called_once_with(expected_url, headers=expected_headers)
@@ -52,7 +55,8 @@ def test_extract_dna_linha(mock_get, mock_auth_token):
 def test_extract_dna_calendar(mock_get, mock_auth_token):
     test_date = '01/01/2023'
     expected_response_data = {'result': 'success'}
-    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA=dna_fake&data={test_date}'
+    name_dna = 'dna_calendario'
+    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?data_inicio={test_date}&data_final={test_date}&nomeDNA={name_dna}'
     expected_headers = {'Authorization': 'Bearer fake_token'}
 
     response_mock = MagicMock()
@@ -60,7 +64,7 @@ def test_extract_dna_calendar(mock_get, mock_auth_token):
     response_mock.json.return_value = expected_response_data
     mock_get.return_value = response_mock
 
-    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/WSLogin', nome_dna='dna_fake')
+    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/BI_Extrator', nome_dna=name_dna)
     actual_response = sianet_extractor.extract_dna_calendar(test_date)
 
     mock_get.assert_called_once_with(expected_url, headers=expected_headers)
@@ -71,7 +75,8 @@ def test_extract_dna_calendar(mock_get, mock_auth_token):
 def test_extract_dna_passageirosTranspArcoSul(mock_get, mock_auth_token):
     test_date = '01/01/2023'
     expected_response_data = {'result': 'success'}
-    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA=dna_fake&data={test_date}'
+    name_dna = 'dna_passageirosTranspArcoSul'
+    expected_url = f'http://siannet.gestaosian.com/api/BI_Extrator?nomeDNA={name_dna}&data={test_date}'
     expected_headers = {'Authorization': 'Bearer fake_token'}
 
     response_mock = MagicMock()
@@ -79,7 +84,7 @@ def test_extract_dna_passageirosTranspArcoSul(mock_get, mock_auth_token):
     response_mock.json.return_value = expected_response_data
     mock_get.return_value = response_mock
 
-    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/WSLogin', nome_dna='dna_fake')
+    sianet_extractor = SianetExtractor(base_url='http://siannet.gestaosian.com/api/WSLogin', nome_dna=name_dna)
     actual_response = sianet_extractor.extract_dna_passageirosTranspArcoSul(test_date)
 
     mock_get.assert_called_once_with(expected_url, headers=expected_headers)
