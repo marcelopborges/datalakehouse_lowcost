54075408c1fc6ce0699c3599514b59525f1c02af
marcelopborges <54992376+marcelopborges@users.noreply.github.com>
2024-01-17 14:46:04 -0300

    refactor(start): processo de melhoria

---
+++ elt/convertion/sianet_convertion.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+import pandas as pd
+
+
+
+class Transformer:
+    def __init__(self, nome_dna, raw_data, date):
+        self.raw_data = raw_data
+        self.nome_dna = nome_dna
+        self.date = date
+
+    def convert_data(self):
+        """
+        Verifica o dado de entrada, converte para um dataframe pandas e chama a função para converter para parquet.
+        :return: o caminho do arquivo gerado já no formato parquet.
+        """
+        if isinstance(self.raw_data, dict) and 'dados' in self.raw_data:
+            df = pd.DataFrame(self.raw_data['dados'])
+        else:
+            raise ValueError("Formato de raw_data não suportado ou chave 'dados' não encontrada.")
+
+        return df
+
+
+    def save_as_parquet(self, df, date):
+        output = BytesIO()
+        df.to_parquet(output, index=False)
+        output.seek(0)  # Retorna ao início do stream
+        date_str = pd.to_datetime(date, dayfirst=True).strftime("%Y%m%d")
+        filename = f"{self.nome_dna}_{date_str}.parquet"
+        return output, filename
+
---
+++ tests/test_sianet_convertion.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import pandas as pd
+import pytest
+from elt.convertion.sianet_convertion import Transformer
+from unittest.mock import patch
+import os
+
+
+@pytest.fixture
+def example_data():
+    return {
+        "dados": [
+            {"coluna1": 1, "coluna2": 2},
+            {"coluna1": 3, "coluna2": 4}
+        ]
+    }
+
+@pytest.fixture
+def date():
+    return datetime.strptime('02/01/2024', '%d/%m/%Y').strftime('%d/%m/%Y')
+
+
+def test_convert_data_with_valid_input(example_data, date):
+    transformer = Transformer("test_dna", example_data, date)
+    result = transformer.convert_data()
+
+    # Cria um DataFrame a partir de example_data['dados'] para a comparação
+    expected_dataframe = pd.DataFrame(example_data['dados'])
+
+    assert not result.empty
+    assert result.equals(expected_dataframe)
